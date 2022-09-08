7af76e120a0abfa5e4bc2bd433dc30bdbf77a3ee
Stefan Koev <st.koev@outlook.com>
2022-09-08 18:09:48 +0300

    implement future date deadline

--- app.js
+++ app.js
@@ -21,3 +21,19 @@ const weekdays = [
   "Friday",
   "Saturday",
 ];
+
+const giveaway = document.querySelector(".giveaway");
+const deadline = document.querySelector(".deadline");
+const items = document.querySelectorAll(".deadline-format h4");
+
+let futureDate = new Date(2022, 9, 18, 9, 50, 0);
+const year = futureDate.getFullYear();
+const hours = futureDate.getHours();
+const minutes = futureDate.getMinutes();
+const month = months[futureDate.getMonth()];
+const date = futureDate.getDate();
+const weekday = weekdays[futureDate.getDay()];
+
+giveaway.textContent = `giveaway ends on ${date} ${month} (${weekday}), ${year} at ${hours}:${minutes} am`;
+
+console.log(date);
